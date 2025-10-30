9761c0ee3f593993963a768096ac209c1d49359d
Jeeva <palanijeeva2004@gmail.com>
2025-10-30 19:52:15 +0530

    Initial commit: Setup backend structure, dependencies.

---
+++ backend/src/main/java/com/foodshare/app/sharebite/ShareBiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foodshare.app.sharebite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareBiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareBiteBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/foodshare/app/sharebite/ShareBiteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodshare.app.sharebite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareBiteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
